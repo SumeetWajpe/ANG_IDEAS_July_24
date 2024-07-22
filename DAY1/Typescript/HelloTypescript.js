ed8b112e90a2e16b1ce4df93450312bc76fb9e1e
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-07-22 10:50:34 +0530

    ✨ Strong Typing

--- DAY1/Typescript/HelloTypescript.js
+++ DAY1/Typescript/HelloTypescript.js
@@ -1,1 +1,7 @@
-console.log("Hello Typescript !");
+// var s = "Hello Typescript !"; // type inferencing
+// s = 100;
+var s = "Hello Typescript !"; // type annotation
+console.log(s);
+var n;
+var b;
+var x;
--- DAY1/Typescript/HelloTypescript.ts
+++ DAY1/Typescript/HelloTypescript.ts
@@ -1,1 +1,26 @@
-console.log("Hello Typescript !");
+// var s = "Hello Typescript !"; // type inferencing
+// s = 100;
+
+var s: string = "Hello Typescript !"; // type annotation
+console.log(s);
+
+var n: number;
+var b: boolean;
+
+var x: any;
+x = 100;
+x = "Bye !";
+x = [10, 20];
+x = { name: "IDEAS", city: "Pune" };
+
+// functions
+function Add(x: number, y: number): number | string {
+  if (x < 0) {
+    return "x should be greater than zero !";
+  }
+  return x + y;
+}
+
+var result: number | string = Add(10, 20);
+
+
