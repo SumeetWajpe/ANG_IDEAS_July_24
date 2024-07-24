d2a8eceebe049bcaaf32831bc6f7850f1089aedd
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-07-24 10:59:07 +0530

    ✨ Using Atomic Design Pattern | Rating

--- DAY3/onlineshopping-app/src/app/app.module.ts
+++ DAY3/onlineshopping-app/src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ListofproductsComponent } from './components/listofproducts/listofproducts.component';
 import { ProductComponent } from './components/product/product.component';
+import { IconComponent } from './components/atoms/icon/icon.component';
+import { RatingComponent } from './components/molecules/rating/rating.component';
 
 @NgModule({
-  declarations: [AppComponent, ListofproductsComponent, ProductComponent],
+  declarations: [AppComponent, ListofproductsComponent, ProductComponent, IconComponent, RatingComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
